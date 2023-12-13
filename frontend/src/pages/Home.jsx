6a7e9b1c80ab87b1f01e2b6d4e7426f1ccf6861b
nikhil <nikhilpadiyath@gmail.com>
2023-12-13 16:37:48 +0530

    Register, login and logout functionality enabled

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import {Link} from "react-router-dom"
+import {FaQuestionCircle, FaTicketAlt} from 'react-icons/fa'
+
+function Home() {
+  return (
+    <>
+    <section className='heading'>
+        <h2>What do you need help with?</h2>
+        <p>Please choose from an option below</p>
+    </section>
+
+    <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create a new ticket
+    </Link>
+
+    <Link to='/tickets' className='btn btn-block'>
+        <FaTicketAlt /> View my tickets
+    </Link>
+    </>
+  )
+}
+
+export default Home
